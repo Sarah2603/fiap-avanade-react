9473c063c25a494092af420e4bf74bfa05e2292c
Sarah2603 <sarahbarreto86@gmail.com>
2022-10-11 12:21:06 -0300

    correção

--- pages/palestra.tsx
+++ pages/palestra.tsx
@@ -1,60 +1,51 @@
 import { NextPageContext } from 'next';
-import React, {useEffect, useState} from 'react'
+import React, {useState, useEffect} from 'react'
 
 type PalestraProps = {
     frase: string;
-    autor: string;
-
+    autor ?:string;
 }
-async function getFrase() {
-    const res = await fetch('https://type.fit/api/quotes' );
-    const data = await res.json();
-    const position = Math.floor(Math.random() * data.length);
-    const frase = data[position].text;
-    const autor = data[position].author;
-    
-    return {
-        frase, autor
-    }
+ async function getFrase(){
+            const res = await fetch('https://type.fit/api/quotes');
+            const data = await res.json();
+            const position =Math.floor(Math.random() * data.length);
+            const frase = data[position].text;
+            const autor = data[position].author;
 
-export default function Palestra(props:PalestraProps){
+            return {
+                frase, autor
+            }
+        }
+export default function Palestra(props:PalestraProps) {
     const [frase, setFrase] = useState<string | undefined>(props.frase)
     const [autor, setAutor] = useState<string | undefined>(props.autor)
- 
-    useEffect(()=>){
-        getFrase().then((data)=>console.log(`executou ${contador} vez`);};
-        contador++;
-    },[contador]);
-        const retorno = getFrase();
-            
-        }
+    let contador = 0;
+    useEffect(()=>{
+
+        getFrase().then((data)=>{ console.log(`Executou ${contador} vez`, data);});
+        contador++;      
        
+    },[frase, autor]);
 
-            return{
-                frase, autor}
+  return (
+  <>
+    <h1>{frase?frase:"Seja muito bem vindo!"}</h1>
+    {autor &&<h2>{autor}</h2>}
 
-            }
-         }
-        
-    }
- 
-    return (
-        <>
-            <h1>{frase?frase:"Seja muito bem vindo!"}</h1>
-         {autor && <h2>{autor}</h2>}
-        <button onClick={(e)=>setAutor("Sarah Barreto")}>Mudar Autor</button>
-        <button onClick={(e)=>setFrase("Quem tem mais, tem dez!")}>Mudar Frase</button>
-        
-        
-        </>
-        )
-    
+    <button onClick={(e)=>setAutor("Sarah Barreto")}>Mudar Autor</button>
+    <button onClick={(e)=>setFrase("Quem tem mais, tem dez!")}>Muda Frase</button>
 
+
+ </>
+  )
+}
+
+//DRY - Don't Repeat Yourself
 Palestra.getInitialProps = async (ctx: NextPageContext) => {
- 
-    const res = await fetch('https://type.fit/api/quotes' );
+
+    const res = await fetch('https://type.fit/api/quotes');
     const data = await res.json();
-    const position = Math.floor(Math.random() * data.length);
+    const position =Math.floor(Math.random() * data.length);
     const frase = data[position].text;
     const autor = data[position].author;
     return {
@@ -62,6 +53,3 @@ Palestra.getInitialProps = async (ctx: NextPageContext) => {
         autor
     }
 }
-
-       
-
