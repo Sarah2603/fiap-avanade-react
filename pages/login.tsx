3b65759ceb7e3c5758da98e1e0035b5d59cc9254
Sarah2603 <sarahbarreto86@gmail.com>
2022-10-11 08:20:23 -0300

    deploy

---
+++ pages/login.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from '../components/Header';
+
+export default function LoginPage() {
+
+  return (
+
+    <>
+
+    <Header pesquisar="Buscar"/>
+
+    <h1>Bem vindo ao Banco Digital</h1>
+
+    </>
+
+  )
+
+}
